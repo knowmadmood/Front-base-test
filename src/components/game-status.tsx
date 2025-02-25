d47645cd71f7067b0e60ee38b13a80b97d61d9fe
edgarcheverier <edgarcheverier@gmail.com>
2025-02-25 12:15:16 +0100

    Move GameStatus to TS

--- src/components/game-status.tsx
+++ src/components/game-status.tsx
@@ -1,16 +1,23 @@
-const GameStatus = ({ hasGuessed, isWinner }) => {
+interface Props {
+  hasGuessed: boolean;
+  isWinner: boolean;
+}
+
+const GameStatus = ({ hasGuessed, isWinner }: Props) => {
   if (hasGuessed && !isWinner)
     return (
       <div className="p-4 text-red-700 bg-red-300 border border-red-900">
         Nope. Sorry.
       </div>
     );
+
   if (hasGuessed && isWinner)
     return (
       <div className="p-4 text-green-700 bg-green-300 border border-green-900">
         Wow, you actually won.
       </div>
     );
+
   return (
     <div className="p-4 border border-cyan-700 bg-cyan-300 text-cyan-900">
       Try and guess, I guess.
